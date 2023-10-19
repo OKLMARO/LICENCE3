825b93ac4a261b0620357e89ba26debf25b55d18
Amairi Omar <omar.amairi@etu.u-pec.fr>
2023-10-20 00:14:35 +0200

    Avancement TM4 Prog Concu

--- Programmation_Concurrente/TM4/exo3.java
+++ Programmation_Concurrente/TM4/exo3.java
@@ -9,11 +9,8 @@ public class exo3 {
         Thread[] threads = new Thread[10];
         for (int i = 0; i < v.length; i++) {
             v[i] = new Voiture(new Random().nextBoolean(), pont);
-            threads[i] = new Thread();
+            threads[i] = new Thread(v[i]);
             threads[i].start();
-        }
-        while (true){
-            
         }
     }
 }
@@ -31,12 +28,20 @@ class Voiture implements Runnable {
     @Override
     public void run() {
         while (true) {
-            pont.enter();
-            System.out.println(Thread.currentThread().getId() + " passe sur le pont");
+            
+            pont.enter(this);
+            
             try{
                 Thread.sleep(new Random().nextInt(50));
             } catch(InterruptedException e){}
+
+            System.out.println("Direction : " + direction);
+            
             pont.exit();
+            
+            try{
+                Thread.sleep(new Random().nextInt(50));
+            } catch(InterruptedException e){}
         }
     }
 }
@@ -44,12 +49,43 @@ class Voiture implements Runnable {
 class Pont {
     boolean isUsed = false;
     boolean direction;
+    int passage_pont = 0;
 
-    public void enter(){
+    public synchronized void enter(Voiture v){
+        if(this.isUsed == true && this.direction != v.direction){
+            try{    
+                this.wait();
+            } catch(InterruptedException e){}
+        }
+        
+        try{
+            Thread.sleep(new Random().nextInt(100));
+        } catch(InterruptedException e){}
+
         this.isUsed = true;
+        this.passage_pont = this.passage_pont + 1;
+
+        System.out.println("La voiture : " + Thread.currentThread().getId() + " passe sur le pont");
     }
 
-    public void exit(){
-        this.isUsed = false;
+    public synchronized void exit(){
+        if(this.passage_pont == 0){
+            this.isUsed = false;
+            this.notify();
+        }
+
+        else if(this.passage_pont == 4){
+            this.passage_pont = 0;
+            
+            if(this.direction == true){
+                this.direction = false;
+            } else{
+                this.direction = true;
+            }
+            
+            this.notifyAll();
+        }
+
+        System.out.println("La voiture : " + Thread.currentThread().getId() + " sors du pont");
     }
 }
