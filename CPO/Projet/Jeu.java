807629bd714a49f90afb6992a565d9bce8366a5b
Omar <mounioma@gmail.com>
2023-10-29 15:30:16 +0100

    CPO Projet Commencement

---
+++ CPO/Projet/Jeu.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class Jeu {
+    public static void main(String[] args) {
+        Frame frame = new Frame("Labyrinthe");
+        frame.setSize(1000, 700);
+        frame.setVisible(true);
+        frame.setBackground(Color.BLACK);
+    }
+}
